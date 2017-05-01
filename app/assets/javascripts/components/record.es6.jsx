90b94bde71e5df08aa6fbe9c9d8a7f2d8f78f0b7
Brendan <tarrnobi@users.noreply.github.com>
2017-05-01 12:14:03 +0100

    Added Delete Functionality

--- app/assets/javascripts/components/record.es6.jsx
+++ app/assets/javascripts/components/record.es6.jsx
@@ -2,13 +2,28 @@ class Record extends React.Component{
   formatAmount(amount) {
     return '$' + Number(amount.toLocaleString())
   }
-  
+  handleDelete(event){
+    event.preventDefault()
+    deleteURL = "/records/" + this.props.record.id
+    $.ajax({
+      type:"DELETE",
+      url: deleteURL,
+      success: () => {this.props.handleDeleteRecord(this.props.record)},
+      dataType: "JSON"
+    })
+  }
   render(){
     return (
       <tr>
         <td>{this.props.title}</td>
         <td>{this.props.date}</td>
         <td>{this.formatAmount(this.props.amount)}</td>
+        <td>
+          <a className="btn btn-danger"
+          onClick={this.handleDelete.bind(this)}>
+          Delete
+          </a>
+        </td>
       </tr>
     )
   }
--- app/assets/javascripts/components/records.es6.jsx
+++ app/assets/javascripts/components/records.es6.jsx
@@ -7,9 +7,14 @@ class Records extends React.Component {
     }
   }
   addRecord(record){
-    console.log("Records: adding new record")
     records = this.state.records.slice()
     records.push(record)
+    this.setState({records: records})
+  }
+  deleteRecord(record){
+    records = this.state.records.slice()
+    index = records.indexOf(record)
+    records.splice(index, 1)
     this.setState({records: records})
   }
   credits(){
@@ -50,25 +55,25 @@ class Records extends React.Component {
       <table className="table table-bordered" >
         <thead>
           <tr>
-            <th >Date</th>
-            <th >Title</th>
-            <th >Amount</th>
+            <th>Date</th>
+            <th>Title</th>
+            <th>Amount</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
         {
-          this.state.records.map(function(row){
+          this.state.records.map((row) => {
             return (
               <Record
                 key={row.id}
                 title={row.date}
                 date={row.title}
                 amount={row.amount}
+                record={row}
+                handleDeleteRecord={this.deleteRecord.bind(this)}
               />
-            )
-
-          })
-
+            )})
         }
       </tbody>
       </table>
