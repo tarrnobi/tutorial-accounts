fb3a26005851f98dc4f6fe7227b3a890c1deb088
Brendan <tarrnobi@users.noreply.github.com>
2017-05-01 11:21:09 +0100

    Split out Components to their own files, added amount box

--- app/assets/javascripts/components/records.es6.jsx
+++ app/assets/javascripts/components/records.es6.jsx
@@ -12,10 +12,38 @@ class Records extends React.Component {
     records.push(record)
     this.setState({records: records})
   }
+  credits(){
+    credits = this.state.records.filter(val => val.amount>=0)
+    return credits.reduce ( (previous, current) => previous + parseFloat(current.amount),0)
+  }
+  debits(){
+    debits = this.state.records.filter(val => val.amount < 0)
+    val    = debits.reduce ((previous, current) => previous + parseFloat(current.amount),0)
+
+    return val
+  }
+  balance(){
+    return this.debits() + this.credits()
+  }
   render () {
     return (
     <div className="records">
       <h2 className="title">Records</h2>
+      <AmountBox
+        type="success"
+        amount={this.credits()}
+        text="Credit" />
+      <AmountBox
+        type="danger"
+        amount={this.debits()}
+        text="Debit" />
+      <AmountBox
+        type="info"
+        amount={this.balance()}
+        text="Balance" />
+
+
+
       <RecordForm
         handleNewRecord={this.addRecord.bind(this)}
       />
@@ -33,8 +61,8 @@ class Records extends React.Component {
             return (
               <Record
                 key={row.id}
-                title={row.title}
-                date={row.date}
+                title={row.date}
+                date={row.title}
                 amount={row.amount}
               />
             )
@@ -46,88 +74,6 @@ class Records extends React.Component {
       </table>
     </div>
     );
-  }
-
-}
-
-class Record extends React.Component{
-  render(){
-    return (
-      <tr>
-        <td>{this.props.title}</td>
-        <td>{this.props.date}</td>
-        <td>{this.props.amount}</td>
-      </tr>
-    )
-  }
-}
-
-const initialState = {
-  title: '',
-  date:  '',
-  amount: '',
-}
-class RecordForm extends React.Component{
-
-  constructor(props){
-    super(props)
-    this.state = initialState
-  }
-  handleChange(event){
-    var change = {}
-    change[event.target.name] = event.target.value
-    this.setState(change)
-  }
-  handleSubmit(event){
-    event.preventDefault()
-    $.post(''
-      , {record: this.state}
-      , (data) => { this.props.handleNewRecord(data) }
-      , 'JSON').done((result) => {this.setState(initialState)})
-
-  }
-  valid(){
-    return this.state.title && this.state.date && this.state.amount
-  }
-  render(){
-    return (
-      <form className="form-inline"
-         onSubmit={this.handleSubmit.bind(this)}>
-        <div className="form-group">
-          <input type="text"
-            className="form-control"
-            placeholder="Date"
-            name="date"
-            value={this.state.date}
-            onChange={this.handleChange.bind(this)}/>
-
-        </div>
-        <div className="form-group">
-          <input type="text"
-            className="form-control"
-            placeholder="Title"
-            name="title"
-            value={this.state.title}
-            onChange={this.handleChange.bind(this)}/>
-        </div>
-        <div className="form-group">
-          <input type="number"
-            className="form-control"
-            placeholder="Amount"
-            name="amount"
-            value={this.state.amount}
-            onChange={this.handleChange.bind(this)}/>
-
-        </div>
-        <button
-          type="submit"
-          className="btn btn-primary"
-          disabled={!this.valid()}
-          onSubmit={this.handleSubmit.bind(this)}>Create Record
-        </button>
-      </form>
-
-    )
   }
 
 }
